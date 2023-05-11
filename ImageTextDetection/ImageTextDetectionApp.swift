55c9d14ff9cb8a587691ba80c4428d0fb8f2bcf1
fyounis-ford <85873467+fyounis-ford@users.noreply.github.com>
2023-05-11 17:26:00 +0100

    Initial Commit

---
+++ ImageTextDetection/ImageTextDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageTextDetectionApp.swift
+//  ImageTextDetection
+//
+//  Created by Younis, Faisal (F.) on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageTextDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
